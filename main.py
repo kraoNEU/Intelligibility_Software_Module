bbf28fe9a0b85a20d9bff1597e56a96bc36b6182
kraoNEU <chelamkuri.v@northeastern.edu>
2022-07-27 17:55:38 -0400

    Fixed the Module Logo and Instruction Logo

--- main.py
+++ main.py
@@ -11,13 +11,20 @@ root.title("Intelligibility Software Module for SMILe Lab")
 # Setting the Window
 root.geometry("600x200+200+200")
 
-# Set the Logo of the Model
-root.iconbitmap("../Logo/Northeastern_University_seal.png")
+# Setting the Instruction Logo
+img = PhotoImage(file='Logo/instruction_icon.png')
+root.tk.call('wm', 'iconphoto', root._w, img)
 
 # Set the Instruction Window
 InstructionWindow(root)
 
-# Calling the SentencePlayer Package From src
+# Set the Logo of the Model
+# root.iconbitmap("/Users/cvkrishnarao/Desktop/RA/Intelligibility_Software_Module/Logo/Northeastern_Huskies_logo.png")
+
+img = PhotoImage(file='/Users/cvkrishnarao/Desktop/RA/Intelligibility_Software_Module/Logo/Northeastern_Huskies_logo.png')
+root.tk.call('wm', 'iconphoto', root._w, img)
+
+# Set the SentencePlayer Window
 SentencesPlayer(root)
 
 # End Main Tkinter window
