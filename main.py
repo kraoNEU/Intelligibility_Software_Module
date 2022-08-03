d332e7f27b699ccb5d52b033cc1da22e2c4a53e6
kraoNEU <chelamkuri.v@northeastern.edu>
2022-08-03 08:59:21 -0400

    Added dynamic File

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
 from tkinter import *
+from tkinter.filedialog import askdirectory
+
 from src.SentencesPlayer import SentencesPlayer
 from src.StartupInstructionWindow import *
 
@@ -10,6 +13,10 @@ root.title("Intelligibility Software Module for SMILe Lab")
 
 # Setting the Window
 root.geometry("600x200+200+200")
+
+scriptDir = os.getcwd()  # directory from where script was ran
+directory = askdirectory()
+os.chdir(directory)
 
 # Setting the Instruction Logo
 img = PhotoImage(file='Logo/instruction_icon.png')
